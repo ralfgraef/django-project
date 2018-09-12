332eed148ee5b23a0c22e1d7d58b2daa2f3188bb
Ralle1968 <graef.ralf@gmail.com>
2018-09-12 15:50:10 +0200

    add bootstrap and superuser

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts= [
+  {
+    'author': 'Ralf Gräf',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'date_posted': 'August 27, 2018'
+  }, 
+  {
+    'author': 'John Smith',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'August 28, 2018'
+  } 
+]
+
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Blog Home</h1>')
+  context = {
+    'posts': posts
+  }
+  return render(request, 'blog/home.html', context)
 
 def about(request):
-  return HttpResponse('<h1>Blog About</h1>')
+  return render(request, 'blog/about.html', {'title': 'About'})
