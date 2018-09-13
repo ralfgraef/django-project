6d5efbddf4a0c2581866ea7e3228765b04bd255c
Ralle1968 <graef.ralf@gmail.com>
2018-09-13 12:29:06 +0200

    add database and entries

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
-
-posts= [
-  {
-    'author': 'Ralf Gräf',
-    'title': 'Blog Post 1',
-    'content': 'First post content',
-    'date_posted': 'August 27, 2018'
-  }, 
-  {
-    'author': 'John Smith',
-    'title': 'Blog Post 2',
-    'content': 'Second post content',
-    'date_posted': 'August 28, 2018'
-  } 
-]
+from .models import Post
 
 
 # Create your views here.
 def home(request):
   context = {
-    'posts': posts
+    'posts': Post.objects.all()
   }
   return render(request, 'blog/home.html', context)
 
